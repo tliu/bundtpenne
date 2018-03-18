7fccd73d83126f297699dd32b25e23f59104446c
Thomas Liu <tmkliu@gmail.com>
2018-03-18 22:18:17 +0000

    remove export

--- export/Player.hx
+++
@@ -1,8 +0,0 @@
-package ;
-import flixel.FlxSprite;
-class Player extends FlxSprite{
-    public function new(x:Float = 0, y:Float = 0) {
-        super(x, y);
-        this.loadGraphic(AssetPaths.bundt__png);
-    }
-}
--- export/windows/neko/bin/assets/data/tiles.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="tiles" tilewidth="16" tileheight="16" tilecount="20" columns="20">
- <image source="../images/tiles.png" width="320" height="16"/>
-</tileset>
